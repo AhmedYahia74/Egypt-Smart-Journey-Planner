f58410ff368c8b74dcd3b7942ca34d3b72bb162f
Camellia Mohamed <camellamohamed321@gmail.com>
2025-02-03 23:58:56 +0200

    Add ValidationTripForm Action

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -1,27 +1,63 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from datetime import datetime
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def fetch_cities_from_database() -> List[Text]:
+    # Replace with database fetching logic
+    return [
+        "luxor",
+        "cairo",
+        "alexandria",
+        "aswan",
+        "aurghada",
+        "aharm el eheikh",
+        "dahab",
+        "marsa alam",
+        "el gouna",
+        "siwa oasis"
+    ]
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+CITIES_NAMES = fetch_cities_from_database()
+FAMILY_STATUS = {
+    'solo': ["solo traveler", "single traveler"],
+    'romantic': ["couple", "honeymoon", "partner"],
+    'family': ["family", "kids", "parents", "siblings", "relatives"],
+    'group trip': ["group", "friends", "colleagues"],
+    'business': ["business", "work", "conference"],
+    'pet-friendly trip': ["pet", "dog", "cat", "animal"]
+}
+
+class ValidateTripForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_trip_form"
+
+    def validate_state(self,
+                       slot_value: Any,
+                       dispatcher: CollectingDispatcher,
+                       tracker: Tracker,
+                       domain: Dict[Text, Any]) -> Dict[Text, Any]:
+
+        if slot_value.lower() in [city.lower() for city in CITIES_NAMES]:
+            return {"state": slot_value}
+        else:
+            dispatcher.utter_message("Sorry, we don't have a trip to this city")
+            return {"state": None}
+
+
+
+    def validate_family_status(self,
+                                slot_value: Any,
+                                dispatcher: CollectingDispatcher,
+                                tracker: Tracker,
+                                domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        for key, value in FAMILY_STATUS.items():
+            if slot_value.lower() in value:
+                return {"family_status": key}
+        return {"family_status": 'General'}
+
+
+
