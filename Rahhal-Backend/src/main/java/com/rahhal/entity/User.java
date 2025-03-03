a7b808dab29fe265c515de42b6fcf2702e1f9553
rwida <rwidajaber@gmail.com>
2025-03-02 20:43:57 -0800

    User Entity & Repository has been created

---
+++ Rahhal-Backend/src/main/java/com/rahhal/entity/User.java
@@ -0,0 +1,75 @@
+package com.rahhal.entity;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Column(name = "role",nullable = false)
+    private String role;
+
+    public User() {
+    }
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
