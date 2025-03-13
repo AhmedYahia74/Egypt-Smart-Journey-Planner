8fce23c450102bca3524b0034f173ad24b1d64e9
AhmedYahia74 <ahmedyahia0114666@gmail.com>
2025-03-13 13:17:39 +0200

    add validation

--- Rahhal-Backend/src/main/java/com/rahhal/entity/User.java
+++ Rahhal-Backend/src/main/java/com/rahhal/entity/User.java
@@ -1,7 +1,10 @@
 package com.rahhal.entity;
 
+import com.rahhal.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.constraints.*;
+
 
 @Entity
 @Table(name = "users")
@@ -13,9 +16,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
+    @Email(message = "Please provide a valid e-mail")
+    @NotNull
+    @Column(unique = true)
     private String email;
 
+    @NotNull
+    @Size(min = 2, message = "password must be at least 2 characters long")
     private String password;
 
-    private String role;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
 }
