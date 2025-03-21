5d9a6fc5acc6139f67a32dc10f0f823a46bec080
Camellia Mohamed <camellamohamed321@gmail.com>
2025-03-21 09:25:22 +0200

    Refactor suggesting cities logic

--- Chatbot/APIs/suggestion_api.py
+++ Chatbot/APIs/suggestion_api.py
@@ -6,7 +6,15 @@ import psycopg2
 
 app = FastAPI()
 
-EMBEDDING_API_URL="http://localhost:9000/empadding"
+EMBEDDING_API_URL="http://localhost:8000/empadding"
+
+def concatenate_user_messages(user_msgs_json):
+    user_msgs_str = ''
+    for key, value in user_msgs_json.items():
+        if value not in user_msgs_str:
+            user_msgs_str += value + ' '
+    return user_msgs_str
+
 @app.get("/suggest_cities")
 def suggest_cities(conversation_id:int):
     conn=None
@@ -20,17 +28,14 @@ def suggest_cities(conversation_id:int):
         if not user_data:
             return {"error":"No conversation found with this conversation id"}
 
-        if not user_data[0]:
-            return {"error":"No user message found in this conversation"}
-        if not user_data[1]:
-            return {"error":"No slot values found in this conversation"}
-        user_msgs,slot_values=user_data[0],user_data[1]
+
+        user_msgs,user_values=concatenate_user_messages(user_data[0]),user_data[1]
         user_msgs_embedding = requests.post(EMBEDDING_API_URL, json={"text": user_msgs}).json()
 
         # get the cities the top 3 matched cities from the database
-        select_query="SELECT city_name, description, embedding <-> %s::vector AS similarity FROM states ORDER BY similarity LIMIT 3"
+        select_query="SELECT name, description, embedding <-> %s::vector AS similarity FROM states ORDER BY similarity LIMIT 3"
 
-        cur.execute(select_query,(str(user_msgs_embedding),))
+        cur.execute(select_query,(str(user_msgs_embedding["empadding"]),))
         cities=cur.fetchall()
         return {"cities":cities}
     except Exception as e:
