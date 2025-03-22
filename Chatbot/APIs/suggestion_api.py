2b3f957b8c18838514b172d4644a3bce9ff0b0c7
Camellia Mohamed <camellamohamed321@gmail.com>
2025-03-22 23:19:04 +0200

    Implement suggestion endpoint for trip

--- Chatbot/APIs/suggestion_api.py
+++ Chatbot/APIs/suggestion_api.py
@@ -6,7 +6,7 @@ import psycopg2
 
 app = FastAPI()
 
-EMBEDDING_API_URL="http://localhost:8000/empadding"
+EMBEDDING_API_URL="http://localhost:8000/embedding"
 
 def concatenate_user_messages(user_msgs_json):
     user_msgs_str = ''
@@ -19,25 +19,17 @@ def concatenate_user_messages(user_msgs_json):
 def suggest_cities(conversation_id:int):
     conn=None
     cur=None
-    print("Conversation ID: ",conversation_id)
     try:
         conn=psycopg2.connect(**DB_Prams)
         cur=conn.cursor()
-        cur.execute("SELECT user_msgs,slot_values FROM conversation_data WHERE conversation_id=%s",(conversation_id,))
-        user_data=cur.fetchone()
-        if not user_data:
-            return {"error":"No conversation found with this conversation id"}
-
-
-        user_msgs,user_values=concatenate_user_messages(user_data[0]),user_data[1]
-        user_msgs_embedding = requests.post(EMBEDDING_API_URL, json={"text": user_msgs}).json()
+        user_msgs_embedding,user_values = get_user_msgs_embedding(conversation_id,cur,conn)
 
         # get the cities the top 3 matched cities from the database
         select_query="SELECT name, description, embedding <-> %s::vector AS similarity FROM states ORDER BY similarity LIMIT 3"
 
-        cur.execute(select_query,(str(user_msgs_embedding["empadding"]),))
-        cities=cur.fetchall()
-        return {"cities":cities}
+        cur.execute(select_query,(str(user_msgs_embedding["embedding"]),))
+        trips=cur.fetchall()
+        return {"trips":trips}
     except Exception as e:
         print(f"Error in founding conversation: {e}")
     finally:
@@ -46,3 +38,42 @@ def suggest_cities(conversation_id:int):
         if conn:
             conn.close()
 
+
+@app.get("/suggest_trips")
+def suggest_trips(conversation_id:int,city_name:str):
+    conn=None
+    cur=None
+
+    try:
+        conn=psycopg2.connect(**DB_Prams)
+        cur=conn.cursor()
+        user_msgs_embedding,user_values = get_user_msgs_embedding(conversation_id,cur,conn)
+        budget=user_values["budget"]
+        start_date=user_values["arrival_date"][0]
+        end_date=user_values["arrival_date"][1]
+        # get the best 3 matched trips from the database considering the city name and trip date
+        select_query=("SELECT *, embedding <-> %s::vector AS similarity FROM trips \
+                    WHERE state= %s AND price <= %s AND date BETWEEN %s AND%s AND is_active = true ORDER BY similarity LIMIT 3")
+        select_prams=(str(user_msgs_embedding["embedding"]),city_name,budget,start_date,end_date)
+
+        cur.execute(select_query,select_prams)
+        trips=cur.fetchall()
+        return {"trips":trips}
+    except Exception as e:
+        print(f"Error in founding conversation: {e}")
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+
+def get_user_msgs_embedding(conversation_id,cur=None,conn=None):
+
+    cur.execute("SELECT user_msgs,slot_values FROM conversation_data WHERE conversation_id=%s",(conversation_id,))
+    user_data=cur.fetchone()
+    if not user_data:
+        return {"error": "No conversation found with this conversation id"}
+    user_msgs, user_values = concatenate_user_messages(user_data[0]), user_data[1]
+    user_msgs_embedding = requests.post(EMBEDDING_API_URL, json={"text": user_msgs}).json()
+    return user_msgs_embedding, user_values
