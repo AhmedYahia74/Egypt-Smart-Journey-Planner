7fc3d7035fee8988c8951ad539fa48949eea9699
rwida <rwidajaber@gmail.com>
2025-06-08 20:38:33 -0700

    tourist-delete-review

--- Rahhal-Backend/src/main/java/com/rahhal/controller/TouristController.java
+++ Rahhal-Backend/src/main/java/com/rahhal/controller/TouristController.java
@@ -3,28 +3,35 @@ package com.rahhal.controller;
 import com.rahhal.dto.ReviewDTO;
 import com.rahhal.service.ReviewService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tourist")
 @PreAuthorize("hasRole('ROLE_TOURIST')")
 public class TouristController {
 
-    private final ReviewService touristService;
+    private final ReviewService reviewService;
 
     public TouristController(ReviewService touristService) {
-        this.touristService = touristService;
+        this.reviewService = touristService;
     }
 
     @PostMapping("/reviews")
-    public ResponseEntity<String> addReview(@Valid @RequestBody ReviewDTO dto)
+    public ResponseEntity<Void> addReview(@Valid @RequestBody ReviewDTO dto)
     {
-        touristService.addReview(dto);
-        return ResponseEntity.ok("Review submitted successfully.");
+        reviewService.addReview(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<Void> deleteReview(@PathVariable int reviewId)
+    {
+        reviewService.deleteReview(reviewId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
