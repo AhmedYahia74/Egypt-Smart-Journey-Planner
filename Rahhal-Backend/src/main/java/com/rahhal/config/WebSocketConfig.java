8c4fafe2b8bf61311118c8ad3b9feb37db2fde91
AhmedYahia74 <ahmedyahia0114666@gmail.com>
2025-06-17 17:20:31 +0300

    create chatbot endpoints

---
+++ Rahhal-Backend/src/main/java/com/rahhal/config/WebSocketConfig.java
@@ -0,0 +1,39 @@
+package com.rahhal.config;
+
+import com.rahhal.security.CustomHandshakeHandler;
+import com.rahhal.security.CustomUserDetailsService;
+import com.rahhal.security.JwtHandshakeInterceptor;
+import com.rahhal.security.JwtService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public WebSocketConfig(JwtService jwtService, CustomUserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chatbot")
+                .addInterceptors(new JwtHandshakeInterceptor(jwtService, userDetailsService))
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
+
